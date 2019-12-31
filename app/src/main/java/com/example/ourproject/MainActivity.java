d4a035ebf30e16f54d05dd10e499ce6985c05982
shimon <shimonaabb@gmail.com>
2019-12-31 15:41:50 +0200

    first page with simanya

--- app/src/main/java/com/example/ourproject/MainActivity.java
+++ app/src/main/java/com/example/ourproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ourproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -14,9 +16,18 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String query = "select sqlite_version() AS sqlite_version";
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(":memory:", null);
+        Cursor cursor = db.rawQuery(query, null);
+        String sqliteVersion = "";
+        if (cursor.moveToNext()) {
+            sqliteVersion = cursor.getString(0);
+        }
 
        /* final Button press = findViewById(R.id.pressButton);
         final TextView resultsTextView = findViewById(R.id.showInput);
