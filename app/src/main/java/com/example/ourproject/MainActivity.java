3760dbb0a5912dcdaa4d35a62ff9e03d38a757d6
shimon <shimonaabb@gmail.com>
2020-01-06 18:33:40 +0200

    with gps

--- app/src/main/java/com/example/ourproject/MainActivity.java
+++ app/src/main/java/com/example/ourproject/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.ourproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,9 +21,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
-
 public class MainActivity extends AppCompatActivity {
 
+    private Location location;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,34 @@ public class MainActivity extends AppCompatActivity {
             sqliteVersion = cursor.getString(0);
         }
 
+        final Button button= (Button)findViewById(R.id.Gps_bt);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                    /*Thread thread = new Thread(){
+                        public void run(){
+                            location = getLastBestLocation();                        }
+                    };
+                    try {
+                        thread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    thread.start();*/
+
+                    location = getLastBestLocation();
+                    button.setText(" "+location.getLongitude()+" , "+location.getLatitude());
+
+                }
+            }
+        });
+
+
        /* final Button press = findViewById(R.id.pressButton);
         final TextView resultsTextView = findViewById(R.id.showInput);
         final EditText inputText = findViewById(R.id.editText);
@@ -40,5 +79,57 @@ public class MainActivity extends AppCompatActivity {
                 resultsTextView.setText("hello "+ inputText.getText());
             }
         });*/
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==1){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                location = getLastBestLocation();
+
+            }
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private Location getLastBestLocation() {
+        LocationManager mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        @SuppressLint("MissingPermission") Location locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            ;
+            return null;
+        }
+
+            Location locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            long GPSLocationTime = 0;
+            if (null != locationGPS) {
+                GPSLocationTime = locationGPS.getTime();
+            }
+
+            long NetLocationTime = 0;
+
+            if (null != locationNet) {
+                NetLocationTime = locationNet.getTime();
+            }
+
+            if (0 < GPSLocationTime - NetLocationTime) {
+                return locationGPS;
+            } else {
+                return locationNet;
+            }
+
     }
 }
